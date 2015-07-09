18dea839c11d0f9c5825e8489804ac92b20585e1
Varghese George <vargheseg5@gmail.com>
2015-07-09 23:49:29 +0530

    Add initial main.py
    Add an initial version of main.py file with a basic list of things to do.

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+	# TODO: Read URL input
+	# TODO: Convert to Grayscale
+	# TODO: Calculate Structural Similarity Index
+	# TODO: Check if SSIM>threshold to determine if they are similar
+
+if __name__ == '__main__':
+	main()
