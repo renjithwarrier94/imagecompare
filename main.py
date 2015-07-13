23be78045f0012b179bb00394bfdd0aaf9177ec6
Rakhul S Prakash <rsp9099@gmail.com>
2015-07-13 18:19:01 +0530

    Add Comment
    Comment is Hello World

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
 	# TODO: Convert to Grayscale
 	# TODO: Calculate Structural Similarity Index
 	# TODO: Check if SSIM>threshold to determine if they are similar
+# Hello world
 
 if __name__ == '__main__':
 	main()
