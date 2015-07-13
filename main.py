da00430d3beb25d9d6a0b5b9831190f88ecfe7e3
sid-ryuzaki <sidharthm007@gmail.com>
2015-07-13 23:07:46 +0530

    Add numpy import and Greyscale weighted pixel function
    Using The weighted formula Grey=R*0.299+G*0.587+B*0.144,
    a weighted greyscale method is  defined.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from urllib.request import urlopen
+import numpy as np
 
 
 def main():
@@ -20,6 +21,12 @@ def main():
 # http://cdn1.vox-cdn.com/assets/4229567/game-of-thrones-poster_85627-1920x1200.jpg
 # http://cdn.playbuzz.com/cdn/54e7fe1b-e96c-485e-b13f-a205635e0642/1dd69c6a-8870-4709-b28e-0d0d21a67500.jpg
 # TODO: Convert to Grayscale
+
+#weighted average of image=R*0.299 + G*0.587 + B*0.114 per pixel
+
+def average(pixel):
+    return 0.299*pixel[0] + 0.587*pixel[1] + 0.114*pixel[2]
+
 # TODO: Calculate Structural Similarity Index
 # TODO: Check if SSIM>threshold to determine if they are similar
 
