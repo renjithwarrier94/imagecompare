57448455610501a5bc967d545841877c2928cd41
sid-ryuzaki <sidharthm007@gmail.com>
2015-07-13 18:26:28 +0530

    Add a conflicting comment
    A conflict

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ def main():
 	# TODO: Convert to Grayscale
 	# TODO: Calculate Structural Similarity Index
 	# TODO: Check if SSIM>threshold to determine if they are similar
-
+    #Conflicting comment
 if __name__ == '__main__':
 	main()
