9ff590acea7310a7287b9b80bc0858fcd4c64122
Rakhul S Prakash <rsp9099@gmail.com>
2015-07-13 21:38:50 +0530

    Add Image URLs
    Add URLs of two Images as Input from User.

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-def main():
-	# TODO: Read URL input
-	# TODO: Convert to Grayscale
-	# TODO: Calculate Structural Similarity Index
-	# TODO: Check if SSIM>threshold to determine if they are similar
+from urllib.request import urlopen
 
-    #Conflicting comment Hello world
+
+def main():
+# TODO: Read URL input
+	f1 = open('1.jpg', 'wb')
+	f2 = open('2.jpg', 'wb')
+	url1 = input('Enter the URL of the first image, please --> ')
+	url2 = input('Enter the URL of the second image, please --> ')
+	f1.write(urlopen(url1).read())
+	print('First Image has downloaded successfully')
+	f2.write(urlopen(url2).read())
+	print('Second Image has downloaded successfully')
+
+	f1.close()
+	f2.close()
+
+
+
+# http://cdn1.vox-cdn.com/assets/4229567/game-of-thrones-poster_85627-1920x1200.jpg
+# http://cdn.playbuzz.com/cdn/54e7fe1b-e96c-485e-b13f-a205635e0642/1dd69c6a-8870-4709-b28e-0d0d21a67500.jpg
+# TODO: Convert to Grayscale
+# TODO: Calculate Structural Similarity Index
+# TODO: Check if SSIM>threshold to determine if they are similar
+
 
 if __name__ == '__main__':
-	main()
+    main()
