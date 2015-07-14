6c08de4582fec91848d72309ff08eccea68dcc78
Renjith Warrier <renjithvwarrier@gmail.com>
2015-07-14 14:06:43 +0530

    Move greyscale2 definition outside if

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ def main():
 # Read URL input
 	f1 = open('1.jpg', 'wb')
 	f2 = open('2.jpg', 'wb')
-	#url1 = input('Enter the URL of the first image, please --> ')
-	#url2 = input('Enter the URL of the second image, please --> ')
-	#f1.write(urlopen(url1).read())
-	f1.write(urlopen('http://cdn1.vox-cdn.com/assets/4229567/game-of-thrones-poster_85627-1920x1200.jpg').read())
+	url1 = input('Enter the URL of the first image, please --> ')
+	url2 = input('Enter the URL of the second image, please --> ')
+	f1.write(urlopen(url1).read())
+	#f1.write(urlopen('http://cdn1.vox-cdn.com/assets/4229567/game-of-thrones-poster_85627-1920x1200.jpg').read())
 	print('First Image has downloaded successfully')
-	#f2.write(urlopen(url2).read())
-	f2.write(urlopen('http://cdn.playbuzz.com/cdn/54e7fe1b-e96c-485e-b13f-a205635e0642/1dd69c6a-8870-4709-b28e-0d0d21a67500.jpg').read())
+	f2.write(urlopen(url2).read())
+	#f2.write(urlopen('http://cdn.playbuzz.com/cdn/54e7fe1b-e96c-485e-b13f-a205635e0642/1dd69c6a-8870-4709-b28e-0d0d21a67500.jpg').read())
 	print('Second Image has downloaded successfully')
 
 	f1.close()
@@ -35,40 +35,59 @@ def comp():
 	image2 = misc.imread('2.jpg')
 	res1 = image1.shape[0]*image1.shape[1]
 	res2 = image2.shape[0]*image2.shape[1]
+	if(len(image1.shape) == 3):
+		g1 = 0
+	else:
+		g1 = 1
+	if(len(image2.shape) == 3):
+		g2 = 0
+	else:
+		g2 = 1
 	if (res1 != res2):
 		if(res1 > res2):
 			size = (image2.shape[0], image2.shape[1])
 			image1 = misc.imresize(image1, size, interp='bicubic', mode=None)
 		else:
 			size = (image1.shape[0], image1.shape[1])
-			image2 = misc.imresize(image2, size, interp='bicubic', mode=None)	
-	grey1 = np.zeros((image1.shape[0], image1.shape[1])) # init 2D numpy array
-	print('Converting 1st image to greyscale....')
-	count = 0
+			image2 = misc.imresize(image2, size, interp='bicubic', mode=None)
 	if(res1 < res2):
 		res = res1
 	else:
-		res = res2
-	for rownum in range(len(image1)):
-		for colnum in range(len(image1[rownum])):
-			grey1[rownum][colnum] = average(image1[rownum][colnum])
-			count = count+1
-			i = (count/res)*100
-			sys.stdout.write("\r%d%%" % i)
-	misc.imsave('grey1.jpg', grey1)
-	sys.stdout.flush()
-
-	print('\n\nConverting 2nd image to greyscale.....')
-	grey2 = np.zeros((image2.shape[0], image2.shape[1])) # init 2D numpy array
-	c = 0
-	for rownum in range(len(image2)):
-		for colnum in range(len(image2[rownum])):
-			grey2[rownum][colnum] = average(image2[rownum][colnum])
-			c+=1
-			i = (c/res)*100
-			sys.stdout.write("\r%d%%" % i)
-	misc.imsave('grey2.jpg', grey2)
-	print('\n\nCompleted!')
+		res = res2	
+	grey1 = np.zeros((image1.shape[0], image1.shape[1])) # init 2D numpy array
+	print('Converting 1st image to greyscale....')
+	if(g1 == 0):
+		count = 0
+		for rownum in range(len(image1)):
+			for colnum in range(len(image1[rownum])):
+				grey1[rownum][colnum] = average(image1[rownum][colnum])
+				count = count+1
+				i = (count/res)*100
+				sys.stdout.write("\r%d%%" % i)
+		misc.imsave('grey1.jpg', grey1)
+		sys.stdout.flush()
+	else:
+		print('Image1 is already greyscale')
+		grey1 = image1
+		print(image1.shape)
+		print(grey1.shape)
+	grey2 = np.zeros((image2.shape[0], image2.shape[1]))
+	if(g2 == 0):
+		print('\n\nConverting 2nd image to greyscale.....')
+		#grey2 = np.zeros((image2.shape[0], image2.shape[1])) # init 2D numpy array
+		c = 0
+		for rownum in range(len(image2)):
+			for colnum in range(len(image2[rownum])):
+				grey2[rownum][colnum] = average(image2[rownum][colnum])
+				c+=1
+				i = (c/res)*100
+				sys.stdout.write("\r%d%%" % i)
+		misc.imsave('grey2.jpg', grey2)
+		print('\n\nCompleted!')
+	else:
+		print('Image2 is already greyscale')
+		grey2 = image2
+	print(grey2.shape)
 	print('Comparing images......')
 	s = ssim(grey1, grey2)
 	s = ((s+1)/2)*100
